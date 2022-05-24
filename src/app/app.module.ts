ba645b27e3059850e44bdff64f0276d5b56e4691
MoD_MoS <s62040626260097@email.kmutnb.ac.th>
2022-05-25 01:05:28 +0700

    Mos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RegisterComponent } from './component/student/register/register.component';
 import { AddSubComponent } from './component/student/add-sub/add-sub.component';
@@ -58,6 +60,9 @@ import { LdataSubjectComponent } from './component/leader/ldata-subject/ldata-su
     MatSidenavModule,
     MatFormFieldModule,
     MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
